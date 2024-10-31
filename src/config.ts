99c24ff6a134dff5d78e0e1d2870e3a95444fa8b
erik <erik@eriksolsen.com>
2024-10-31 18:12:49 +0000

    init

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+export const imageConfig = {
+	imageSizes: [4000, 3000, 2000, 1800, 1400, 1200, 1000, 900, 800, 700, 600, 500, 250],
+};
+
+export const siteConfig = {
+	name: "Atlas Broncos",
+	nameShort: "Atlas",
+	phone: {
+		base: "+15126090471",
+		formatted: "(512) 609-0471",
+	},
+	email: {
+		base: "erik@eriksolsen.com",
+	},
+	url: "https://atlasbroncos.com",
+	description: "Atlas Broncos",
+};
