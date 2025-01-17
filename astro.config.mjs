988cf8917eeae1cb711f5f7b55ad136c95aab178
erik <erik@eriksolsen.com>
2025-01-17 22:21:19 +0000

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import alpinejs from '@astrojs/alpinejs';
 import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
 	adapter: netlify(),
-	integrations: [alpinejs(), tailwind({ applyBaseStyles: true })],
+	integrations: [alpinejs(), sitemap(), tailwind({ applyBaseStyles: true })],
 	site: 'https://atlasbroncos.com',
 });
