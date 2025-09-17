e461fdfb3648e1ef1a03ecf4c56eda995a65f98d
erik <erik@eriksolsen.com>
2025-09-17 15:49:35 +0000

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,23 @@ import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
-	adapter: netlify(),
-	experimental: {
-		fonts: [
-			{
-				provider: fontProviders.google(),
-				name: "Manrope",
-				cssVariable: "--font-manrope",
-				weights: ["200 800"],
-			}
-		],
-	},
-	integrations: [alpinejs(), sitemap()],
-	site: 'https://atlasbroncos.com',
-	vite: {
-		plugins: [tailwindcss()]
-	},
+    adapter: netlify(),
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "Manrope",
+                cssVariable: "--font-manrope",
+                weights: ["200 800"],
+            }
+        ],
+    },
+    integrations: [alpinejs(), sitemap(), partytown()],
+    site: 'https://atlasbroncos.com',
+    vite: {
+        plugins: [tailwindcss()]
+    },
 });
